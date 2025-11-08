8b9ae586dddf4b2019d1d0becbb4730f26b3c5ba
Britney Hawthorne <britneyhawthorne@Britneys-MacBook-Air.local>
2025-11-08 15:01:02 -0500

    Layout

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector('.newsletter form');
+    const emailEntered = document.getElementById('email');
+    const messageDisplay = document.querySelector('.message');
+
+    emailEntered.setAttribute("type", "text");
+    form.addEventListener('submit', function(e) {
+        e.preventDefault(); 
+
+        const email = emailEntered.value.trim().toLowerCase();
+
+        if (email !== "" && email.includes("@") && email.includes(".com")) {
+            messageDisplay.textContent = `Thank you! Your email address "${email}" has been added to our mailing list!`;
+            form.reset();
+        } else {
+            messageDisplay.textContent = "Please enter a valid email address.";
+        }
+    });
+});
